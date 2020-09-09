ba0ee6dc417f7eea0e0a0fca48c56dc5927c53b7
gordmaxi <gloriavibo1@gmail.com>
2020-09-09 12:59:40 +0200

    Abiturklassen

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
 
-    private ServerFrame serverFrame;
-    private Server server;
     public static void main(String[] args) {
         new Main();
     }
-    public Main(){
 
+
+    public Main(){
     }
 }
--- src/ServerFrame.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class ServerFrame {
-    private JPasswordField passwordField1;
-    private JTextField portTextField;
-    private JButton startButton;
-    private JButton closeButton;
-    private JTextArea Log;
-}
