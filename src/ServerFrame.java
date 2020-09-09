d92de027eea75afbc5f61cc54f0dfee2140db3de
Batmanderes <ugur_akin42@emailn.de>
2020-09-09 12:45:23 +0200

    Frame erstellt, Abiturklasse gelöscht

---
+++ src/ServerFrame.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class ServerFrame {
+    private JPasswordField passwordField1;
+    private JTextField portTextField;
+    private JButton startButton;
+    private JButton closeButton;
+}
