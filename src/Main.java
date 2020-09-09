1f9ef96fa7f1342035a63f069aea2f1c4ae2a52a
Batmanderes <ugur_akin42@emailn.de>
2020-09-09 13:10:14 +0200

    Frame in main initialisiert

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,17 @@ import javax.swing.*;
 
 public class Main {
 private Server server;
+private JPanel jPanel;
     public static void main(String[] args) {
         new Main();
-        new Server();
+
+
     }
 
 
     public Main(){
+        jPanel = new JPanel();
         server = new Server();
+
     }
 }
